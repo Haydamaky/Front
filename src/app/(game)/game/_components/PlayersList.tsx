75ea2fc0c30cc6bcd5f33d58464eb2aade393293
Taras Yurchenko <turchenko730@gmail.com>
2024-12-17 21:24:17 +0200

    feat: made players for mobile version

--- src/app/(game)/game/_components/PlayersList.tsx
+++ src/app/(game)/game/_components/PlayersList.tsx
@@ -7,10 +7,20 @@ import { useEffect, useRef, useState } from 'react';
 import { Avatar } from '@nextui-org/react';
 import { getUserInfo } from '@/store/slices/user';
 
+type PlayerColor = 'white' | 'black' | 'blue' | 'red' | 'yellow';
+
+interface Player {
+  id: number;
+  color: PlayerColor;
+  userId: string;
+  money: number;
+  user: { nickname: string };
+}
+
 const PlayersList = () => {
   const dispatch = useAppDispatch();
   const game = useAppSelector(state => state.game.game);
-  const user = useAppSelector(state => state.user);
+  const [usersTurnId, setUsersTurnId] = useState('');
   const [turnTime, setTurnTime] = useState(0);
   const intervalRef = useRef<null | NodeJS.Timeout>(null);
   useEffect(() => {
@@ -18,7 +28,6 @@ const PlayersList = () => {
     const startCountdown = (timeToEnd: any) => {
       let countDown = timeToEnd;
       if (intervalRef.current) clearInterval(intervalRef.current);
-
       intervalRef.current = setInterval(() => {
         if (countDown <= 0) {
           if (intervalRef.current) clearInterval(intervalRef.current);
@@ -30,18 +39,26 @@ const PlayersList = () => {
     };
 
     socket.emit('getGameData', ({ game, fields }: any) => {
-      console.log(game);
       dispatch(setGame(game));
       const now = Date.now();
       const timeToEnd = Math.floor((+game.turnEnds - now) / 1000);
       setTurnTime(timeToEnd);
       startCountdown(timeToEnd);
+      setUsersTurnId(game.turnOfUserId);
     });
 
-    socket.on('tradedField', (data: any) => {
+    socket.on('hasPutUpForAuction', (data: any) => {
       const now = Date.now();
       const timeToEnd = Math.floor((+data.turnEnds - now) / 1000);
       setTurnTime(timeToEnd);
+      startCountdown(timeToEnd);
+    });
+
+    socket.on('passTurnToNext', (data: any) => {
+      const now = Date.now();
+      const timeToEnd = Math.floor((+data.turnEnds - now) / 1000);
+      setTurnTime(timeToEnd);
+      setUsersTurnId(data.turnOfNextUserId);
       startCountdown(timeToEnd);
     });
 
@@ -53,7 +70,6 @@ const PlayersList = () => {
     });
 
     socket.on('error', (err: any) => console.log(err));
-
     return () => {
       if (intervalRef.current) clearInterval(intervalRef.current);
       socket.off('rolledDice');
@@ -62,26 +78,41 @@ const PlayersList = () => {
       socket.off('tradedField');
     };
   }, []);
+  const colorVariants: Record<string, string> = {
+    pink: 'border-pink-500',
+    green: 'border-green-500',
+    blue: 'border-blue-500',
+    red: 'border-red-500',
+    yellow: 'border-yellow-500',
+  };
+
   return (
     <div className="flex h-full flex-col justify-between py-2 text-xs">
-      {game.players?.map(player => (
-        <div
-          key={player.id}
-          className="relative flex h-[23%] flex-col items-center justify-center bg-pink-400"
-        >
-          {game.turnOfUserId === player.userId && (
-            <div className="absolute right-[5%] top-[-2%] rounded-[0.150rem] bg-base p-[2px]">
-              90
-            </div>
-          )}
-          <Avatar
-            size="md"
-            src="https://i.pravatar.cc/150?u=a04258114e29026302d"
-          />
-          <p>{player.user.nickname}</p>
-          <p>{player.money}$</p>
-        </div>
-      ))}
+      {game.players?.map((player: Player) => {
+        const borderColor = colorVariants[player.color];
+        const highLigthedShadow =
+          player.userId === usersTurnId
+            ? 'shadow-[0px_0px_4px_3px_rgba(255,247,0,1)]'
+            : '';
+        return (
+          <div
+            key={player.id}
+            className={`bg-playerCard ${borderColor} ${highLigthedShadow} relative flex h-[23%] flex-col items-center justify-center overflow-hidden border-2 border-solid`}
+          >
+            {player.userId === usersTurnId && (
+              <div className="absolute right-[5%] top-[-2%] rounded-[0.150rem] bg-base px-1 py-[2px] text-primary">
+                {turnTime}
+              </div>
+            )}
+            <Avatar
+              size="md"
+              src="https://i.pravatar.cc/150?u=a04258114e29026302d"
+            />
+            <p>{player.user.nickname}</p>
+            <p>{player.money}$</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
