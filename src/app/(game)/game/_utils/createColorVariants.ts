20d23afbb0081d4b1f60978d5d44cfb44564e9db
Taras Yurchenko <turchenko730@gmail.com>
2024-12-28 00:39:44 +0200

    fix: fixed colors in tailwind

--- src/app/(game)/game/_utils/createColorVariants.ts
+++ src/app/(game)/game/_utils/createColorVariants.ts
@@ -1,16 +1,16 @@
 export const createColorVariants = (property: string, suffix: string) =>
   ({
-    pink: `bg-pink-${suffix}`,
-    green: `bg-green-${suffix}`,
-    blue: `bg-blue-${suffix === '700' ? '700' : '300'}`,
-    red: `bg-red-${suffix}`,
-    yellow: `bg-yellow-${suffix}`,
-    'dark-blue': `bg-blue-${suffix}`,
-    purple: `bg-purple-${suffix}`,
-    orange: `bg-orange-${suffix}`,
-    gray: `bg-gray-${suffix}`,
-    black: 'bg-black',
-    tortoise: `bg-teal-${suffix}`,
+    pink: `${property}-pink-${suffix}`,
+    green: `${property}-green-${suffix}`,
+    blue: `${property}-blue-${suffix}`,
+    red: `${property}-red-${suffix}`,
+    yellow: `${property}-yellow-${suffix}`,
+    'dark-blue': `${property}-blue-${suffix}`,
+    purple: `${property}-purple-${suffix}`,
+    orange: `${property}-orange-${suffix}`,
+    gray: `${property}-gray-${suffix}`,
+    black: `${property}-black`,
+    tortoise: `${property}-teal-${suffix}`,
   }) as Record<string, string>;
 
 export const colorVariatsBorder500 = createColorVariants('border', '500');
