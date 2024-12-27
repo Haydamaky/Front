5356304b1dda8516e4e4f82e75b50f6d66676029
Taras Yurchenko <turchenko730@gmail.com>
2024-12-27 15:27:27 +0200

    fix: fixed display of buttons

--- src/app/(game)/game/_components/Center.tsx
+++ src/app/(game)/game/_components/Center.tsx
@@ -22,7 +22,6 @@ const Center = () => {
     field => field.index === player?.currentFieldIndex,
   );
   useEffect(() => {
-    const handleError = (err: any) => console.log(err);
     const handleRolledDice = (data: any) => {
       setAction('buy');
     };
@@ -42,13 +41,11 @@ const Center = () => {
       dispatch(setGame(data.game));
     };
 
-    socket.on('error', handleError);
     socket.on('rolledDice', handleRolledDice);
     socket.on('hasPutUpForAuction', handleHasPutUpForAuction);
     socket.on('passTurnToNext', handlePassTurnToNext);
     socket.on('boughtField', handleBoughtField);
     return () => {
-      socket.off('error', handleError);
       socket.off('rolledDice', handleRolledDice);
       socket.off('hasPutUpForAuction', handleHasPutUpForAuction);
       socket.off('passTurnToNext', handlePassTurnToNext);
@@ -63,63 +60,64 @@ const Center = () => {
   };
   return (
     <div className="flex h-full flex-col justify-between">
-      {(game.turnOfUserId === user?.id || action === 'auction') && (
-        <div className="mx-6 mt-6 flex h-1/4 flex-col items-center justify-between rounded-md bg-[#FBFBFA] py-2 text-xs text-primary lg:py-3">
-          <div className="text-small md:text-standard lg:text-lg xl:text-2xl">
-            {action === 'rollDice'
-              ? 'Ваш хід'
-              : action === 'buy'
-                ? 'Придбати'
-                : action === 'auction'
-                  ? 'Аукціон'
-                  : ''}
+      {(game.turnOfUserId === user?.id || action === 'auction') &&
+        (!currentField.specialField || action === 'rollDice') && (
+          <div className="mx-6 mt-6 flex h-1/4 flex-col items-center justify-between rounded-md bg-[#FBFBFA] py-2 text-xs text-primary lg:py-3">
+            <div className="text-small md:text-standard lg:text-lg xl:text-2xl">
+              {action === 'rollDice'
+                ? 'Ваш хід'
+                : action === 'buy'
+                  ? 'Придбати'
+                  : action === 'auction'
+                    ? 'Аукціон'
+                    : ''}
+            </div>
+            {action === 'rollDice' && (
+              <Button
+                size={screenSize.width > 1200 ? 'default' : 'xs'}
+                className="font-custom text-[9px] text-white md:text-sm lg:text-lg"
+                onClick={rollDice}
+              >
+                Кинути кубики
+              </Button>
+            )}
+            {action === 'buy' && (
+              <div className="flex gap-1 lg:gap-4">
+                <Button
+                  size={screenSize.width > 1200 ? 'default' : 'xs'}
+                  className="font-custom text-[9px] text-white md:text-sm lg:text-lg"
+                  onClick={buyField}
+                >
+                  Придбати за {currentField.price}
+                </Button>
+                <Button
+                  variant={'white'}
+                  size={screenSize.width > 1200 ? 'default' : 'xs'}
+                  className="font-custom text-[9px] md:text-sm lg:text-lg"
+                >
+                  Виставити на аукціон
+                </Button>
+              </div>
+            )}
+            {action === 'auction' && (
+              <div className="flex gap-1 lg:gap-4">
+                <Button
+                  size={screenSize.width > 1200 ? 'default' : 'xs'}
+                  className="font-custom text-[9px] text-white md:text-sm lg:text-lg"
+                >
+                  Підняти на 100
+                </Button>
+                <Button
+                  variant={'white'}
+                  size={screenSize.width > 1200 ? 'default' : 'xs'}
+                  className="font-custom text-[9px] md:text-sm lg:text-lg"
+                >
+                  Відмовитись
+                </Button>
+              </div>
+            )}
           </div>
-          {action === 'rollDice' && (
-            <Button
-              size={screenSize.width > 1200 ? 'default' : 'xs'}
-              className="font-custom text-[9px] text-white md:text-sm lg:text-lg"
-              onClick={rollDice}
-            >
-              Кинути кубики
-            </Button>
-          )}
-          {action === 'buy' && (
-            <div className="flex gap-1 lg:gap-4">
-              <Button
-                size={screenSize.width > 1200 ? 'default' : 'xs'}
-                className="font-custom text-[9px] text-white md:text-sm lg:text-lg"
-                onClick={buyField}
-              >
-                Придбати за {currentField.price}
-              </Button>
-              <Button
-                variant={'white'}
-                size={screenSize.width > 1200 ? 'default' : 'xs'}
-                className="font-custom text-[9px] md:text-sm lg:text-lg"
-              >
-                Виставити на аукціон
-              </Button>
-            </div>
-          )}
-          {action === 'auction' && (
-            <div className="flex gap-1 lg:gap-4">
-              <Button
-                size={screenSize.width > 1200 ? 'default' : 'xs'}
-                className="font-custom text-[9px] text-white md:text-sm lg:text-lg"
-              >
-                Підняти на 100
-              </Button>
-              <Button
-                variant={'white'}
-                size={screenSize.width > 1200 ? 'default' : 'xs'}
-                className="font-custom text-[9px] md:text-sm lg:text-lg"
-              >
-                Відмовитись
-              </Button>
-            </div>
-          )}
-        </div>
-      )}
+        )}
 
       <div>Chat</div>
     </div>
