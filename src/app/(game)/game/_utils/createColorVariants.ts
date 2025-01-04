cfcf124332f361a2875744af84a5afb11b24d847
Taras Yurchenko <turchenko730@gmail.com>
2025-01-04 20:22:15 +0200

    feat: made players border

--- src/app/(game)/game/_utils/createColorVariants.ts
+++ src/app/(game)/game/_utils/createColorVariants.ts
@@ -13,6 +13,13 @@ const createColorVariants = (property: string, suffix: string) =>
     tortoise: `${property}-teal-${suffix}`,
   }) as Record<string, string>;
 
+export const gradientColorVariants = {
+  green: 'linear-gradient(180deg, #57EBDE 0%, #AEFB2A 100%)',
+  yellow: 'linear-gradient(180deg, #FF930F 0%, #FFF95B 100%)',
+  blue: 'linear-gradient(180deg, #0D41E1 0%, #07C8F9 100%)',
+  purple: 'linear-gradient(180deg, #A106F4 0%, #E707FA 100%)',
+} as Record<string, string>;
+
 export const colorVariatsBorder500 = createColorVariants('border', '500');
 export const colorVariats500 = createColorVariants('bg', '500');
 export const colorVariats700 = createColorVariants('bg', '700');
