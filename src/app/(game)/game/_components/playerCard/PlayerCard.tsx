b9f557a80474497184916ec3c7a5606f868f6ebe
Taras Yurchenko <turchenko730@gmail.com>
2025-01-10 21:31:11 +0200

    feat: added closing of player on click outside

--- src/app/(game)/game/_components/playerCard/PlayerCard.tsx
+++ src/app/(game)/game/_components/playerCard/PlayerCard.tsx
@@ -1,7 +1,7 @@
+import { useRef, useState, useEffect } from 'react';
+import { useAppSelector } from '@/hooks/store';
 import { Player } from '@/types/player';
 import { gradientColorVariants } from '../../_utils';
-import { useState } from 'react';
-import { useAppSelector } from '@/hooks/store';
 import GiveUpButton from './GiveUpButton';
 import InnerPlayerCard from './InnerPlayerCard';
 import { OtherPlayerButtons } from './OtherPlayerButtons';
@@ -23,6 +23,7 @@ const PlayerCard = ({
 }: PlayerCardProps) => {
   const { data: user } = useAppSelector(state => state.user);
   const [isPlayerClicked, setIsPlayerClicked] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
 
   const borderColor = gradientColorVariants[player.color];
   const playersTurn = player.userId === turnOfUserId;
@@ -35,6 +36,19 @@ const PlayerCard = ({
     isPlayerClicked && !mainPlayer ? 'opacity-100' : 'opacity-0';
 
   const handleMouseClick = () => setIsPlayerClicked(prev => !prev);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+        setIsPlayerClicked(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const playerButtons = mainPlayer ? (
     <GiveUpButton opacity={opacity} />
@@ -53,15 +67,17 @@ const PlayerCard = ({
   );
 
   const height = isPlayerClicked ? (mainPlayer ? '25%' : '31%') : '';
+
   return (
     <div
+      ref={cardRef}
       onClick={handleMouseClick}
       style={{
         background: borderColor,
         zIndex: isPlayerClicked ? '1000' : '',
         height,
       }}
-      className={`${fromTop} trasnsition-all font-ermilov absolute flex h-[24%] w-full cursor-pointer flex-col items-center justify-center overflow-hidden rounded-[6%] font-thin duration-300 ease-in-out`}
+      className={`${fromTop} trasnsition-all absolute flex h-[24%] w-full cursor-pointer flex-col items-center justify-center overflow-hidden rounded-[6%] font-ermilov font-thin duration-300 ease-in-out`}
     >
       {player.userId === turnOfUserId ? (
         <div className="h-[98%] w-[97%] rounded-[6%] bg-transparent">
