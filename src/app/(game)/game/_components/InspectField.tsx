794961cd2aaec3af5f015148bd18bc074903e10b
Taras Yurchenko <turchenko730@gmail.com>
2025-01-19 17:56:32 +0200

    feat: made switching positions for fiedls

--- src/app/(game)/game/_components/InspectField.tsx
+++ src/app/(game)/game/_components/InspectField.tsx
@@ -1,20 +1,35 @@
 import { Field } from '@/types/field';
+import { findIfCloseToCorner } from '../_utils/findIfCloseToCorner';
+import { fieldPositionCoors } from '../_utils';
 
 const cornersPositions = {
   'upper-left': 'left-[17%] top-[20%]',
   'bottom-left': 'left-[17%] bottom-[20%]',
   'upper-right': 'right-[17%] top-[20%]',
   'bottom-right': 'right-[17%] bottom-[20%]',
-};
+} as Record<string, string>;
 
 interface InspectFieldProps {
   field: Field;
 }
 
 const InspectField = ({ field }: InspectFieldProps) => {
+  const corner = findIfCloseToCorner(field.index);
+  let position = '';
+  const indexInFieldPositionsArray = field.index - 1;
+  position = fieldPositionCoors[indexInFieldPositionsArray];
+  const isHorizonatlField =
+    field.index < 11 || (field.index > 20 && field.index < 31);
+  let translate = isHorizonatlField
+    ? 'translate-x-[-40%]'
+    : 'translate-y-[-40%]';
+  if (corner) {
+    position = cornersPositions[corner];
+    translate = '';
+  }
   return (
     <div
-      className={`absolute bottom-[20%] right-[17%] h-[30%] w-[17%] bg-purple-600`}
+      className={`absolute ${position} h-[30%] w-[17%] bg-purple-600 ${translate}`}
     >
       {field.name}
     </div>
---
+++ src/app/(game)/game/_utils/findIfCloseToCorner.ts
@@ -0,0 +1,28 @@
+export function findIfCloseToCorner(index: number) {
+  const corners = new Map([
+    [1, 'upper-left'],
+    [11, 'upper-right'],
+    [21, 'bottom-right'],
+    [31, 'bottom-left'],
+  ]);
+
+  const upperLeftExtra = new Set([38, 39, 40]);
+  if (upperLeftExtra.has(index)) {
+    return 'upper-left';
+  }
+
+  const cornerEntries = Array.from(corners.entries());
+  for (const [cornerIndex, cornerName] of cornerEntries) {
+    if ((index >= 1 && index <= 11) || (index >= 21 && index <= 31)) {
+      if (Math.abs(index - cornerIndex) <= 1) {
+        return cornerName;
+      }
+    } else {
+      if (Math.abs(index - cornerIndex) <= 3) {
+        return cornerName;
+      }
+    }
+  }
+
+  return null;
+}
