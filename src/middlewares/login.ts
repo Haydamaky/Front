2645b1df5b4f382fb0bdec03f0886bf6941d0113
Taras Yurchenko <turchenko730@gmail.com>
2025-02-21 15:48:38 +0200

    feat: added logs for cookies

--- src/middlewares/login.ts
+++ src/middlewares/login.ts
@@ -3,9 +3,14 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export const loginMiddleware: MiddlewareFactory = next => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
-    const hasAccessToken = request.cookies.has('access_token');
-    if (!hasAccessToken)
+    const cookies = request.headers.get('cookie');
+    const hasAccessToken = cookies?.includes('access_token=');
+    console.log('hasAccessToken:', hasAccessToken);
+    console.log('cookies:', cookies);
+    if (!hasAccessToken) {
       return NextResponse.redirect(new URL('/login', request.url));
+    }
+
     return next(request, _next);
   };
 };
