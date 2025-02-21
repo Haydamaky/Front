0f3a0f13b1fcb3ee33b4d139c725ea828de5e372
Taras Yurchenko <turchenko730@gmail.com>
2025-02-21 16:18:02 +0200

    fix: commented middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
-import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+// import { cookies } from 'next/headers';
+// import { NextRequest, NextResponse } from 'next/server';
 
-import { stackMiddlewares } from '@/middlewares/stackHandler';
-import { loginMiddleware } from '@/middlewares/login';
-import { gameMiddleware } from './middlewares/game';
+// import { stackMiddlewares } from '@/middlewares/stackHandler';
+// import { loginMiddleware } from '@/middlewares/login';
+// import { gameMiddleware } from './middlewares/game';
 
-const middlewares = [loginMiddleware, gameMiddleware];
-export default stackMiddlewares(middlewares);
+// const middlewares = [loginMiddleware, gameMiddleware];
+// export default stackMiddlewares(middlewares);
 
-export const config = {
-  matcher: ['/rooms', '/game'],
-};
+// export const config = {
+//   matcher: ['/rooms', '/game'],
+// };
