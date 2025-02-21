9557c8b93d0f69d323c244e4447248d4fe2e66e6
Taras Yurchenko <turchenko730@gmail.com>
2025-02-21 16:26:57 +0200

    fix: commented login middleware

--- src/middlewares/login.ts
+++ src/middlewares/login.ts
@@ -1,16 +1,16 @@
-import { MiddlewareFactory } from '@/types/factory';
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+// import { MiddlewareFactory } from '@/types/factory';
+// import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-export const loginMiddleware: MiddlewareFactory = next => {
-  return async (request: NextRequest, _next: NextFetchEvent) => {
-    const cookies = request.headers.get('cookie');
-    const hasAccessToken = cookies?.includes('access_token=');
-    console.log('hasAccessToken:', hasAccessToken);
-    console.log('cookies:', cookies);
-    if (!hasAccessToken) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+// export const loginMiddleware: MiddlewareFactory = next => {
+//   return async (request: NextRequest, _next: NextFetchEvent) => {
+//     const cookies = request.headers.get('cookie');
+//     const hasAccessToken = cookies?.includes('access_token=');
+//     console.log('hasAccessToken:', hasAccessToken);
+//     console.log('cookies:', cookies);
+//     if (!hasAccessToken) {
+//       return NextResponse.redirect(new URL('/login', request.url));
+//     }
 
-    return next(request, _next);
-  };
-};
+//     return next(request, _next);
+//   };
+// };
