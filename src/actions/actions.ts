e3c7dbaab342f33799524ffce063c4ed923bc664
Taras Yurchenko <turchenko730@gmail.com>
2025-02-21 22:06:06 +0200

    fix: deleted domain from cookie

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -4,7 +4,6 @@ import { cookies } from 'next/headers';
 export const setGameIdCookie = async (gameId: string) => {
   const cookiesStore = cookies();
   cookiesStore.set('gameId', gameId, {
-    domain: '.plankton-app-sfddt.ondigitalocean.app',
     maxAge: 7 * 24 * 60 * 60,
     sameSite: 'none',
     httpOnly: false,
