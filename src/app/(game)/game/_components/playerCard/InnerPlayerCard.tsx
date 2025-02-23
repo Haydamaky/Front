3dc50ebc67f3066b36c4053e3d86697d09dbd62b
Taras Yurchenko <turchenko730@gmail.com>
2025-02-23 21:43:59 +0200

    fix: fixed jumping zero

--- src/app/(game)/game/_components/playerCard/InnerPlayerCard.tsx
+++ src/app/(game)/game/_components/playerCard/InnerPlayerCard.tsx
@@ -26,12 +26,14 @@ const InnerPlayerCard = ({
       key={player.id}
       className={`${playerBg} trasnsition-all flex h-full w-full flex-col items-center overflow-clip rounded-[6%] pt-8 duration-300 ease-in-out`}
     >
-      {player.userId === turnOfUserId && turnTime && turnTime >= 0 && (
+      {player.userId === turnOfUserId && turnTime && turnTime >= 0 ? (
         <div
           className={`absolute right-[5%] top-[-0.2vh] flex h-[3.3vh] w-[21%] items-center justify-center rounded-[0.150rem] bg-[#F0F7FF] text-primaryGame ${inter.className} font-normal`}
         >
           {turnTime}
         </div>
+      ) : (
+        ''
       )}
       {!player.lost ? (
         <div className="h-[5rem] w-[5rem]">
