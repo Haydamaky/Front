958d9b2a8c76c6cb4f4c93d5fd7b8bf83a981e1e
Taras Yurchenko <turchenko730@gmail.com>
2025-10-20 13:21:18 +0300

    fix: progress bar

--- src/app/(game)/game/_components/Auction/ProgressBar.tsx
+++ src/app/(game)/game/_components/Auction/ProgressBar.tsx
@@ -13,8 +13,8 @@ const ProgressBar: FC<ProgressBarProps> = ({ auction }) => {
   const [value, setValue] = useState(timeToEnd);
   const [startTime, setStartTime] = useState<number>(Date.now());
   const animationId = useRef<number | null>(null);
+  console.log({ auction });
   useEffect(() => {
-    setValue(timeToEnd);
     setStartTime(Date.now());
     if (animationId.current) cancelAnimationFrame(animationId.current);
 
@@ -35,7 +35,7 @@ const ProgressBar: FC<ProgressBarProps> = ({ auction }) => {
     return () => {
       if (animationId.current) cancelAnimationFrame(animationId.current);
     };
-  }, [auction]);
+  }, [timeToEnd]);
   return (
     <div className="flex w-[95%] flex-col items-center gap-2">
       <span className="mb-4 flex w-14 items-center justify-center rounded-lg border-2 border-white bg-primaryGame p-2 px-2 font-ermilov text-4xl">
